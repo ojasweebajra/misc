16ac8cc60acd5f83abf1c125f70d10f2d13d660b
ojasweebajra <ojaswee1@gmail.com>
2018-12-29 13:11:41 +0000

    First commit

---
+++ OneDrive/desk/coincount/coincount.py
@@ -0,0 +1,10 @@
+
+
+coinFile=open("coin_stuff.txt","r") 
+coinType = input("What type of coin do you have?: ")
+weight = int(input("Weight of bag?: "))
+
+bagFile = open("bag_stuff.txt","r")
+
+found = False 
+found2 = False
